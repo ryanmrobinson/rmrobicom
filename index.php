84e6d7be7de66c428d772af9649755badc95ab64
ryan <ryan.macmaster.robinson@gmail.com>
2013-09-20 09:18:01 -0400

    initial add of files

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+  <link href='http://fonts.googleapis.com/css?family=Lobster|Raleway' rel='stylesheet' type='text/css'>
+    <script src="scripts/jquery-2.0.3.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/index.css">
+  </head>
+  <body>
+    <div class="header">
+	  <h1>Ryan Robinson</h1>
+	  <h3>Software Developer</h3>
+	</div>
+	<div id="mainContent">
+	</div>
+  </body>
+</html>
